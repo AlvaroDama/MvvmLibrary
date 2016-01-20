f716643441b405f7614d7e12d1de584a419abdb1
Álvaro <alvaro.d.jimenez@gmail.com>
2016-01-20 11:48:41 +0100

    MVVM Library with essential MVVM architecture.

---
+++ MvvmLibrary/Module/AutofacModule.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using MvvmLibrary.Factorias;
+using Xamarin.Forms;
+
+namespace MvvmLibrary.Module
+{
+    public class AutofacModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ViewFactory>().As<IViewFactory>().SingleInstance(); //singleton
+
+            builder.RegisterType<Navigator>().As<INavigator>().SingleInstance();
+
+            builder.Register(ctx => Application.Current.MainPage.Navigation).SingleInstance();
+        }
+    }
+}
